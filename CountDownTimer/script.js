17ee3b3311a7a07dcfcfdff3ebadb9b01eea2a86
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-09-08 16:33:13 +0300

    New year count down (#12)
    * count down timer created
    
    * alarm added to the count down timer

--- CountDownTimer/script.js
+++ CountDownTimer/script.js
@@ -2,6 +2,7 @@ const day = document.querySelector('.days');
 const hours = document.querySelector(".hour");
 const minutes = document.querySelector(".minute");
 const seconds = document.querySelector(".second");
+const audioM = document.querySelector('#play');
 
 const newYears = "11 sep 2022";
 
@@ -21,7 +22,25 @@ const countDownTimer = () => {
     hours.innerText = formatTime(hour);
     minutes.innerText = formatTime(minute);
     seconds.innerText = formatTime(second);
-    
+
+    let d = day.innerText = formatTime(days);
+    let h = hours.innerText = formatTime(hour);
+    let m = minutes.innerText = formatTime(minute);
+    let s = seconds.innerText = formatTime(second);
+    // console.log(s);
+    if (s == 00 && m == 00 && h == 0 && d == 0) {
+        try {
+            sound();
+            setTimeout(() => {
+                sound();
+            }, 3000);
+            setTimeout(() => {
+                sound();
+            }, 6000);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 function formatTime(time) {
@@ -32,4 +51,7 @@ function formatTime(time) {
     }
 }
 
+async function sound() {
+    await audioM.play();
+}
 setInterval(countDownTimer, 1000);
