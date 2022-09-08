79bf23a9c341c4c191635ec4685ebfbd648c3ca1
Kaleb Mesfin <kaleabmesfinq@gmail.com>
2022-09-08 16:29:47 +0300

    alarm added to the count down timer

--- CountDownTimer/script.js
+++ CountDownTimer/script.js
@@ -2,6 +2,8 @@ const day = document.querySelector('.days');
 const hours = document.querySelector(".hour");
 const minutes = document.querySelector(".minute");
 const seconds = document.querySelector(".second");
+const audioM = document.querySelector('#play');
+
 
 const newYears = "11 sep 2022";
 
@@ -21,7 +23,25 @@ const countDownTimer = () => {
     hours.innerText = formatTime(hour);
     minutes.innerText = formatTime(minute);
     seconds.innerText = formatTime(second);
-    
+
+    let d = day.innerText = formatTime(days);
+    let h = hours.innerText = formatTime(hour);
+    let m = minutes.innerText = formatTime(minute);
+    let s = seconds.innerText = formatTime(second);
+    // console.log(s);
+    if (s == 00 && m == 00 && h == 0 && d == 0) {
+        try {
+            sound();
+            setTimeout(() => {
+                sound();
+            }, 3000);
+            setTimeout(() => {
+                sound();
+            }, 6000);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 function formatTime(time) {
@@ -30,6 +50,10 @@ function formatTime(time) {
     } else {
         return time;
     }
+}
+
+async function sound() {
+    await audioM.play();
 }
 
 setInterval(countDownTimer, 1000);
